322a800fb51b07a2deb41b2591fe72100cf58eeb
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2023-11-02 17:28:58 +0100

    refactor(database): Review database creation script

--- database/docker-entrypoint-initdb.d/create_tables.sql
+++ database/docker-entrypoint-initdb.d/create_tables.sql
@@ -1,40 +1,40 @@
 USE pale_emploi;
 
-CREATE TABLE IF NOT EXISTS Candidat(
+CREATE TABLE IF NOT EXISTS code_parrainage(
     id INT AUTO_INCREMENT,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    email VARCHAR(50),
-    password VARCHAR(50),
+    code VARCHAR(6) NOT NULL UNIQUE,
+    date_creation DATE,
+    date_affiliation DATE,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS Employeur(
+CREATE TABLE IF NOT EXISTS personne(
     id INT AUTO_INCREMENT,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    password VARCHAR(50),
-    email VARCHAR(50),
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    mot_de_passe VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     entreprise VARCHAR(50),
-    codeParrainage VARCHAR(50),
-    PRIMARY KEY(id)
+    code_parrainage INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(code_parrainage) REFERENCES code_parrainage(id)
 );
 
-CREATE TABLE IF NOT EXISTS Emplois(
+CREATE TABLE IF NOT EXISTS offre_emploi(
     id INT AUTO_INCREMENT,
-    titre VARCHAR(50),
+    titre VARCHAR(50) NOT NULL,
     description VARCHAR(50),
     ville VARCHAR(50),
-    datePublication DATE,
-    employeurId INT NOT NULL,
+    date_publication DATE,
+    employeur_id INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(employeurId) REFERENCES Employeur(id)
+    FOREIGN KEY(employeur_id) REFERENCES personne(id)
 );
 
-CREATE TABLE IF NOT EXISTS Postuler(
-    candidatId INT,
-    emploisId INT,
-    PRIMARY KEY(candidatId, emploisId),
-    FOREIGN KEY(candidatId) REFERENCES Candidat(id),
-    FOREIGN KEY(emploisId) REFERENCES Emplois(id)
+CREATE TABLE IF NOT EXISTS application(
+    candidat INT,
+    emploi INT,
+    PRIMARY KEY(candidat, emploi),
+    FOREIGN KEY(candidat) REFERENCES personne(id),
+    FOREIGN KEY(emploi) REFERENCES offre_emploi(id)
 );
